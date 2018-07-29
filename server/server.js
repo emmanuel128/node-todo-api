5ba94bf9fe52e4feb46cd9cc53e5024c1c80ce2b
emmanuel128 <emmanuel.castro@upr.edu>
2018-07-29 17:02:07 -0400

    Add token method

--- server/server.js
+++ server/server.js
@@ -107,11 +107,13 @@ app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-    user.save().then((doc) => {
-        res.send(doc);
-    }, (error) => {
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user)
+    }).catch((error) => {
         res.status(400).send(error);
-    });
+    })
 })
 
 app.listen(PORT, () => {
