39f9e2a649794e8a846834eecf81d25fa2ba6144
emmanuel128 <emmanuel.castro@upr.edu>
2018-07-31 15:47:14 -0400

    hash password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -69,6 +70,23 @@ UserSchema.statics.findByToken = function (token) {
 
     return User.findOne({ _id: decoded._id, 'tokens.token': token, 'tokens.access': 'auth' });
 };
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) { console.log(err) }
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) { console.log(err) }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
